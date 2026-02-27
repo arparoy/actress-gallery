0f8180b7018ed69a3044396a9a9398835b6d55d1
Arpa Roy <arparoycollection@gmail.com>
2026-02-27 10:58:53 +0600

    Update icon URLs to use '1.jpg'

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/1.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/1.jpg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/1.jpg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/1.jpg',
   },
 }
 
